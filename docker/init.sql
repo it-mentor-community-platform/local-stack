17ef8f46d6e90c1ce95985f4a235bf1f8b709388
Andrey <aprishlov@gmail.com>
2026-07-27 17:08:27 +0300

    feat: добавид interval-repetition-service в local-stack и изменил внешний порт kafka-ui

--- docker/init.sql
+++ docker/init.sql
@@ -4,3 +4,5 @@ CREATE SCHEMA IF NOT EXISTS project_service;
 CREATE SCHEMA IF NOT EXISTS mentor_service;
 CREATE SCHEMA IF NOT EXISTS job_market_analytics_service;
 CREATE SCHEMA IF NOT EXISTS telegram_bot_adapter;
+CREATE SCHEMA IF NOT EXISTS interval_repetition_service;
+
